da5bb4938a7f7bb161a13dfe3340a971014b96d3
CorruptX <corrupt2112@gmail.com>
2021-04-07 18:53:09 -0500

    made gifencorder a comment temporarily

--- commands/upgrade.js
+++ commands/upgrade.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const jimp = require("jimp");
 const fs = require("fs");
-const gifencoder = require("gifencoder");
+//const gifencoder = require("gifencoder");
 const {createCanvas, loadImage} = require("canvas");
 
 module.exports.run = async (client, message, args, prefix, player, db) => {
