07a4acae621a8dff53cb0a0ee6bd4509c7560273
CorruptX <corrupt2112@gmail.com>
2021-04-07 17:35:12 -0500

    temporary comment added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const fs = require('fs');
+/*const fs = require('fs');
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -10,7 +10,7 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
-}
+}*/
 //Login
 client.on('ready', () => {
     console.log('Beycord is now online!')
