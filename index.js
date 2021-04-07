c09aaa73ecb9637e8e12ccfbb1914f855a1e5391
CorruptX <corrupt2112@gmail.com>
2021-04-07 18:26:20 -0500

    Removed duplicate code

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const client = new Discord.Client()
 const fs = require('fs');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
