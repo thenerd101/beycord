494dc246593a5625ec1837ebc9a59f436b272ed9
CorruptX <corrupt2112@gmail.com>
2021-04-07 00:58:29 -0500

    Made beycord working

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-/*
-The beginning of open-source Beycord.
-Waiting for someone to write it.
-Thanks! Test 123
-*/
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require(./config.json)
+
+client.on('ready', () => {
+    console.log('Beycord is now online!')
+})
+
+client.login(config.token)
