925853ad2db267875a01b732998b54476f53ff76
CorruptX <corrupt2112@gmail.com>
2021-04-07 16:49:33 -0500

    more secure token

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require(./config.json)
 
 client.on('ready', () => {
     console.log('Beycord is now online!')
 })
 
-client.login(config.token)
+client.login(process.env.BEYCORD_TOKEN)
