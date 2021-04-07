e96fd3f003e981e4d41d99a0c69f81d37481a5d2
CorruptX <corrupt2112@gmail.com>
2021-04-07 17:23:09 -0500

    Updated for command handler

---
+++ commands/ching.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'ching',
+	description: 'Ching!',
+	execute(message, args) {
+		message.channel.send('chong but im not racist');
+	},
+};
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const fs = require('fs');
 
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+//Login
 client.on('ready', () => {
     console.log('Beycord is now online!')
 })
-
 client.login(process.env.BEYCORD_TOKEN)
