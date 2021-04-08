dfbf3d5c5c6e72e6144957cccb045331ad8d5249
CorruptX <corrupt2112@gmail.com>
2021-04-08 16:37:00 -0500

    temp hide code

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name || command.help.name, command);
 }
-
+/*
 //Eris Bot Command Handler
 var db = {}
 client.on('messageCreate', (msg) => {
@@ -22,6 +22,7 @@ client.on('messageCreate', (msg) => {
 		message.reply('Something happened while trying to run this command :/');
 	}
 });
+*/
 //Login
 client.on('ready', () => {
 	console.log('Beycord is online!')
