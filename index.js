84ec351c7a00c304abafd790b746152939d141b6
CorruptX <corrupt2112@gmail.com>
2021-04-07 23:08:16 -0500

    switching to eris

--- index.js
+++ index.js
@@ -4,29 +4,31 @@ const client = new Discord.Client()
 const fs = require('fs');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const Eris = require("eris-additions")(require("eris"))
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name || command.help.name, command);
 }
-//Command Handler
-var db = {}
 
-client.on('message', message => {
-if (!message.content.startsWith(prefix) || message.author.bot) return;
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-	if (!client.commands.has(command)) return;
+//Eris Bot Command Handler
+bot.on("messageCreate", (msg) => {
+    if(msg.content.startsWith(prefix) || message.author.bot) {
+		const args = message.content.slice(prefix.length).trim().split(/ +/);
+		const command = args.shift().toLowerCase();
+		if (bot.commands.has(command)) return;
 	try {
 		client.commands.get(command).run(client, message, args, prefix, {}, db);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
 	}
+    }
 });
-//Login
-client.on('ready', () => {
-    console.log('Beycord is now online!')
-})
 
-client.login(process.env.TOKEN_DJS)
+//login
+var bot = new Eris("BOT_TOKEN");
+bot.on("ready", () => {
+    console.log("Ready!");
+});
+bot.connect(process.env.TOKEN_DJS)
