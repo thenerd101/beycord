77d2287e2c98645b365995fe33bdc72447bcacac
CorruptX <corrupt2112@gmail.com>
2021-04-07 22:13:12 -0500

    execute --> run

--- index.js
+++ index.js
@@ -11,19 +11,17 @@ for (const file of commandFiles) {
 }
 //Command Handler
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+if (!message.content.startsWith(prefix) || message.author.bot) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 	if (!client.commands.has(command)) return;
 	try {
-		client.commands.get(command).execute(message, args);
+		client.commands.get(command).run(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
 	}
 });
-
-
 //Login
 client.on('ready', () => {
     console.log('Beycord is now online!')
