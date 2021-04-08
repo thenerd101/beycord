05374c19015b2127c6d3d01568f28c63c84f8735
CorruptX <corrupt2112@gmail.com>
2021-04-08 17:10:15 -0500

    fixed command handler

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js')
 const { prefix } = require('./config.json');
 const fs = require('fs');
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const Eris = require("eris-additions")(require("eris"))
-const client = new Eris(process.env.TOKEN_DJS);
+const client = new Eris(process.env.TOKEN_DJS)
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));;
 client.commands = new Discord.Collection();
-
+//this thing
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name || command.help.name, command);
@@ -13,15 +13,15 @@ for (const file of commandFiles) {
 //Eris Bot Command Handler
 var db = {}
 client.on('messageCreate', (msg) => {
-	if (msg.content.startsWith(prefix) || message.author.bot) return;
-	try {
-		client.commands.get(command).run(client, message, args, prefix, {}, db);
-	} catch (error) {
-		console.error(error);
-		message.reply('Something happened while trying to run this command :/');
+	if (msg.content === "!ping") {
+		try {
+			client.commands.get(command).run(client, message, args, prefix, {}, db);
+		} catch (error) {
+			console.error(error);
+			message.reply('Something happened while trying to run this command :/');
+		}
 	}
 });
-
 //Login
 client.on('ready', () => {
 	console.log('Beycord is online!')
