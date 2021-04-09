29c0dfbf2a4987756e30c379ecb400cb540aed74
SunSOG <39193864+SunSOG@users.noreply.github.com>
2021-04-09 09:50:49 +0800

    Fixed index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js')
 const { prefix } = require('./config.json');
 const fs = require('fs');
-const Eris = require("eris-additions")(require("eris"))
-const client = new Eris(process.env.TOKEN_DJS)
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));;
+const Eris = require("eris-additions")(require("eris"));
+const client = new Eris(process.env.TOKEN_DJS);
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 client.commands = new Discord.Collection();
 //this thing
 for (const file of commandFiles) {
@@ -11,19 +11,20 @@ for (const file of commandFiles) {
 	client.commands.set(command.name || command.help.name, command);
 }
 //Eris Bot Command Handler
-var db = {}
-client.on('messageCreate', (msg) => {
-	if (msg.content.startsWith(prefix)) {
-		try {
-			client.commands.get(command).run(client, message, args, prefix, {}, db);
-		} catch (error) {
-			console.error(error);
-			message.reply('Something happened while trying to run this command :/');
-		}
+var db = {};
+client.on('messageCreate', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+	try {
+		client.commands.get(command).run(client, message, args, prefix, {}, db);
+	} catch (error) {
+		console.error(error);
+		message.reply('Something happened while trying to run this command :/');
 	}
 });
 //Login
 client.on('ready', () => {
-	console.log('Beycord is online!')
-})
+	console.log('Beycord is online!');
+});
 client.connect()
