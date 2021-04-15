93d71ffc505ed00b70a628d66ec948bcde496ae3
CorruptX <corrupt2112@gmail.com>
2021-04-14 19:59:07 -0500

    fix hopefully?

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ mongo.connect((err) => {
 
 //commandFiles
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name || command.help.name, command);
