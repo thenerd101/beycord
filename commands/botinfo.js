e0add73d712bd474237f99f393298f9ea3d3537b
CorruptX <corrupt2112@gmail.com>
2021-04-18 23:06:11 -0500

    Added botinfo command

---
+++ commands/botinfo.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (client, message, args, prefix, player, db) => {
+  let startembed = new Discord.MessageEmbed()
+  .setTitle('Information')
+  .setDescription('Information about Beycord')
+  .setColor("#7f7fff")
+  .addFields(
+    { name: 'Author of Beycord', value: 'SunSOG/CorruptX', inline:true},
+    { name: 'Version of Beycord', value: 'Beycord V2.0', inline:true},
+    { name: 'Server Type', value: 'N/A (as of 4/19/21)', inline:true},
+    { name: 'Server Type', value: 'N/A (as of 4/19/21)', inline:true}
+)
+  .setTimestamp()
+  .setFooter('Thank you to StarDust for making Beycord possible in the early days.');
+  message.channel.createMessage({embed:startembed});
+}
+
+module.exports.help = {
+  name: "botinfo",
+  desc: "Information about the bot."
+}
